bbb4becdef2babc4ee9f4390a9a72dafad4c0998
Jay <jlaura@usgs.gov>
2018-11-29 06:08:51 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='spaetzle',
+    version='0.1.0',
+    long_description='',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    scripts=[]
+)
