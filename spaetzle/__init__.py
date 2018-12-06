0058f5af2c6feb14739292690fadaa66020b31e6
Jay <jlaura@usgs.gov>
2018-12-05 20:38:44 -0700

    Generalizes search for usgscsm

--- spaetzle/__init__.py
+++ spaetzle/__init__.py
@@ -7,6 +7,6 @@ from csmapi import csmapi
 
 # Register the usgscam plugin with the csmapi
 
-lib = ctypes.CDLL(os.path.abspath(os.path.join(sysconfig.get_python_lib(), '../../libusgscsm.so')))
+lib = ctypes.CDLL(ctypes.util.find_library('usgscsm.so'))
 if not lib:
     warnings.warn('Unable to load usgscsm shared library')
