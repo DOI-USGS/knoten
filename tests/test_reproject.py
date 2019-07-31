a45b998f0c6e0a45deac670238720ab3e941650a
Kelvinrr <Kelvinrr@icloud.com>
2019-07-31 12:11:44 -0700

    get travis to work

--- tests/test_reproject.py
+++ tests/test_reproject.py
@@ -1,10 +1,10 @@
 from unittest import mock
 import pytest
 
-from knoten import util
+from knoten import utils
 
 def test_reproject():
     with mock.patch('pyproj.transform', return_value=[1,1,1]) as mock_pyproj:
-        res = util.reproject([1,1,1], 10, 10, 'geocent', 'latlon')
+        res = utils.reproject([1,1,1], 10, 10, 'geocent', 'latlon')
         mock_pyproj.assert_called_once()
         assert res == (1,1,1)
