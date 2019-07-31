c3c8b5a7ae39d8be7c9216eaee6428c968f675dc
Kelvinrr <Kelvinrr@icloud.com>
2019-07-31 11:27:05 -0700

    test reproject not pointing to autocnet

--- tests/test_reproject.py
+++ tests/test_reproject.py
@@ -1,10 +1,10 @@
 from unittest import mock
 import pytest
 
-from autocnet.transformation import spatial
+from knoten import util
 
 def test_reproject():
     with mock.patch('pyproj.transform', return_value=[1,1,1]) as mock_pyproj:
-        res = spatial.reproject([1,1,1], 10, 10, 'geocent', 'latlon')
+        res = util.reproject([1,1,1], 10, 10, 'geocent', 'latlon')
         mock_pyproj.assert_called_once()
         assert res == (1,1,1)
