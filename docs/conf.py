38383b45d675fdc9280b378281b69c919a3e7e02
Jacob Cain <jrcain@usgs.gov>
2023-09-06 00:07:14 -0700

    ocaps header/footer for sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -44,9 +44,52 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_material'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_js_files = ['usgsbanner.js']
+html_css_files = [
+    '_static/common.css',
+    '_static/custom.css',
+    '_static/uswds_header.css'
+]
+
+html_sidebars = {
+    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
+}
+
+# Material theme options (see theme.conf for more information)
+html_theme_options = {
+
+    # Set the name of the project to appear in the navigation.
+    'nav_title': 'Knoten',
+
+    # Set you GA account ID to enable tracking
+    # 'google_analytics_account': 'UA-XXXXX',
+
+    # Specify a base_url used to generate sitemap.xml. If not
+    # specified, then no sitemap will be built.
+    # 'base_url': 'https://github.com/DOI-USGS/knoten',
+    
+    'master_doc' : 'False',
+    'heroes' : [],
+    # Set the color and the accent color
+    'color_primary': '#000000',
+    # 'color_accent': 'Red',
+    'theme_color' : '#3f51b5',
+    'logo_icon': '&#xe87a',
+
+    # Set the repo location to get a badge with stats
+    'repo_url': 'https://github.com/DOI-USGS/knoten',
+    'repo_name': 'Project',
+
+    # Visible levels of the global TOC; -1 means unlimited
+    'globaltoc_depth': 3,
+    # If False, expand all TOC entries
+    'globaltoc_collapse': False,
+    # If True, show hidden TOC entries
+    'globaltoc_includehidden': False,
+}
