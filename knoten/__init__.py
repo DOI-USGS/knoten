377bd23afc10340e0015248e3d627fa411b95603
Laura, Jason R <jlaura@usgs.gov>
2024-03-22 14:12:08 -0700

    Changes glob for backwards compatibility

--- knoten/__init__.py
+++ knoten/__init__.py
@@ -12,7 +12,8 @@ if not libusgscsm_path:
     usgscsm_folder = os.path.join(os.path.split(libcsmapi_path)[0], "csmplugins")
     libusgscsm_path = ""
     if os.path.exists(usgscsm_folder):
-        results = glob("*[0-9].[0-9].[0-9].dylib", root_dir=usgscsm_folder)
+        # Supports py < 3.10, if only supporting 3.10+ use: glob( "*[0-9].[0-9].[0-9].dylib", root_dir=usgscsm_folder)
+        results = glob(os.path.join(usgscsm_folder, "*[0-9].[0-9].[0-9].dylib"))
         results.sort()
         libusgscsm_path = os.path.join(usgscsm_folder, results[-1])
 
